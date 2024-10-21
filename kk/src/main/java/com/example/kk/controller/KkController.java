9e78730149b53f1f5f4e9c82480f15f5a54556d2
kimura_ryuto <kimura.ryuto@alhinc.jp>
2024-10-21 10:29:56 +0900

    トップ画面の作成

--- kk/src/main/java/com/example/kk/controller/KkController.java
+++ kk/src/main/java/com/example/kk/controller/KkController.java
@@ -3,10 +3,33 @@ package com.example.kk.controller;
 import com.example.kk.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class KkController {
 
     @Autowired
     TaskService taskService;
+
+    @GetMapping("/top")
+    public ModelAndView top() {
+        ModelAndView mav = new ModelAndView();
+//        // タスクを全件取得処理
+//        List<ReportForm> contentData = reportService.findAllReport();
+        // 画面遷移先を指定
+        mav.setViewName("/top");
+//        // タスクデータオブジェクトを保管
+//        mav.addObject("contents", contentData);
+
+        //本日の日付の取得
+        Date date = new Date();
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd");
+        String today = sdFormat.format(date);
+        mav.addObject("today", today);
+        return mav;
+    }
 }
