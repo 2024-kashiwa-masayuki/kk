0794a1cddff16f65fb979db5db25824aad14eeec
kashiwa_masayuki <kashiwa.masayuki@alhinc.jp>
2024-10-21 10:54:47 +0900

    タスク削除のダイアログを追加

--- kk/src/main/java/com/example/kk/controller/KkController.java
+++ kk/src/main/java/com/example/kk/controller/KkController.java
@@ -4,15 +4,22 @@ import com.example.kk.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class KkController {
 
     @Autowired
     TaskService taskService;
-
+    
     /*
      * タスク削除処理
      */
