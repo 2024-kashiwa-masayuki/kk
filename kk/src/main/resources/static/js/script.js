8b57eaeba577d82632c90faab9bbaca02351cfc2
kashiwa_masayuki <kashiwa.masayuki@alhinc.jp>
2024-10-23 13:48:33 +0900

    画面レイアウトを変更

---
+++ kk/src/main/resources/static/js/script.js
@@ -0,0 +1,53 @@
+window.onload = function(){
+    today = new Date();
+    var elements = document.getElementsByClassName("limit-date");
+    var length = elements.length
+    // 締切判定
+    while (elements.length > 0) {
+        // タスク期限の日付を取得
+        var limitDate = new Date(elements[0].textContent);
+        var limitYear = limitDate.getFullYear();
+        var limitMonth = limitDate.getMonth() + 1;
+        var limitDay = limitDate.getDate();
+        // 今日の日付を取得
+        var year = today.getFullYear();
+        var month = today.getMonth() + 1;
+        var day = today.getDate();
+        // タスク期限が翌日以降なら黄色、当日以前なら赤色で表示
+        if (year == limitYear) {
+            if (month == limitMonth) {
+                if (day < limitDay) {
+                    elements[0].className = 'has-allowance';
+                } else {
+                    elements[0].className = 'deadline-up-close';
+                }
+            }
+            else {
+                if (month < limitMonth) {
+                    elements[0].className = 'has-allowance';
+                } else {
+                    elements[0].className = 'deadline-up-close';
+                }
+            }
+        } else {
+            if (year < limitYear) {
+                elements[0].className = 'has-allowance';
+            } else {
+                elements[0].className = 'deadline-up-close';
+            }
+        }
+    }
+}
+
+function CheckDelete(){
+    let result = window.confirm("タスクを削除してもよろしいですか？");
+    if (!result) return false;
+}
+
+function ChangeStatus(){
+    let result = window.confirm("ステータスを変更してもよろしいですか？");
+    if (!result) {
+        alert('ステータスは変更されませんでした')
+        return false;
+    }
+}
